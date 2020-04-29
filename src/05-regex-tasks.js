b7f6483a1101270dfd79f0e71562928b1064aa17
Ihar <mauler1987@gmail.com>
2020-04-29 08:10:19 +0300

    feat: solution drom 5 to 7 tasks (after weekend work)

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -32,7 +32,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  return new RegExp(/^{[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}}$/);
 }
 
 
@@ -54,7 +54,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return new RegExp(/^[sr]|([p]i)/);
 }
 
 
@@ -78,8 +78,9 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  const pattern = `^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.[a-zA-Z0-9]{${minLength},})`;
+  return new RegExp(pattern);
 }
 
 
